fee0d8797581d1b9137553edcaa337193058740e
Jefferson Carpenter <jeffersoncarpenter2@gmail.com>
2015-11-07 14:32:57 -0600

    en-bebasneue Contact Us submit button

--- public/app.js
+++ public/app.js
@@ -5921,7 +5921,9 @@ define('contactUsView', [
 		})),
 		bodyColumn(componentStream(state.map(text))),
 		bodyColumn(alignLRM({
-			left: submitButton(black, text('Submit')).all([
+			left: submitButton(black, text('Submit').all([
+				fonts.bebasNeue,
+			])).all([
 				link,
 				clickThis(function (ev, disable) {
 					var enable = disable();
