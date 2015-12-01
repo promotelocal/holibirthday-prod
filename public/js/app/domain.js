dc683264fa523f4d3cef13aff3a9108556710da7
Jefferson Carpenter <jeffersoncarpenter2@gmail.com>
2015-11-30 20:12:31 -0600

    change domain

--- public/js/app/domain.js
+++ public/js/app/domain.js
@@ -1,5 +1,5 @@
 define([], function () {
 	// return 'http://71.89.76.184';
-	return 'https://glacial-earth-6398.herokuapp.com';
+	// return 'https://glacial-earth-6398.herokuapp.com';
 	return 'https://www.holibirthday.com';
 });
