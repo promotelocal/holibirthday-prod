e582f7f0008840cc7a502c211b9c54246cad27c6
Jefferson Carpenter <jeffersoncarpenter2@gmail.com>
2015-11-30 19:45:47 -0600

    remove .gitmodules     switch to glacial-earhc
    Tested-by: Jefferson Carpenter <jeffersoncarpenter2@gmail.com>

--- public/app.js
+++ public/app.js
@@ -2935,6 +2935,7 @@ define('areYouSure', [
 });
 define('domain', [], function () {
 	// return 'http://71.89.76.184';
+	return 'https://glacial-earth-6398.herokuapp.com';
 	return 'https://www.holibirthday.com';
 });
 define('app', [
