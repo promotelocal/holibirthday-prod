ef98a41c1230f3c7ec3d0e6a7d7c7b081c45f081
Jefferson Carpenter <jeffersoncarpenter2@gmail.com>
2015-12-07 20:05:35 -0600

    change domain

--- public/app.js
+++ public/app.js
@@ -2979,6 +2979,7 @@ define('areYouSure', [
 	};
 });
 define('domain', [], function () {
+	return 'https://holibirthday.aoeu2code.com';
 	// return 'http://71.89.76.184';
 	// return 'https://glacial-earth-6398.herokuapp.com';
 	return 'https://www.holibirthday.com';
