f8d5bf0752c96d3a2f476ae5293e7289942c8f78
Jefferson Carpenter <jeffersoncarpenter2@gmail.com>
2015-12-07 23:16:51 -0600

    change domain

--- public/js/app/domain.js
+++ public/js/app/domain.js
@@ -1,4 +1,5 @@
 define([], function () {
+	return 'https://nodejs-holibirthday.rhcloud.com/';
 	return 'https://holibirthday.aoeu2code.com';
 	// return 'http://71.89.76.184';
 	// return 'https://glacial-earth-6398.herokuapp.com';
