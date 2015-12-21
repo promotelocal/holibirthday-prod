2702fc52d4f99abb81d11ff5f170542d6d60eeca
Jefferson Carpenter <jeffersoncarpenter2@gmail.com>
2015-12-21 11:59:46 -0600

    fix doma in

--- public/app.js
+++ public/app.js
@@ -3116,7 +3116,7 @@ define('domain', [], function () {
 	// return 'http://localhost';
 	// return 'https://nodejs-holibirthday.rhcloud.com';
 	// return 'https://holibirthday.aoeu2code.com';
-	return 'http://71.89.76.184';
+	// return 'http://71.89.76.184';
 	// return 'https://glacial-earth-6398.herokuapp.com';
 	return 'https://www.holibirthday.com';
 });
