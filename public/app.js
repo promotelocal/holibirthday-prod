b56ec3f90d4356b7e915f7650127b8c8c6a1253d
Jefferson Carpenter <jeffersoncarpenter2@gmail.com>
2016-01-21 14:03:39 -0600

    change domain back and add test thing

--- public/app.js
+++ public/app.js
@@ -3117,7 +3117,7 @@ define('areYouSure', [
 });
 define('domain', [], function () {
 	// return 'http://localhost';
-	return 'https://holibirthdayproduction-holibirthday.rhcloud.com';
+	// return 'https://holibirthdayproduction-holibirthday.rhcloud.com';
 	// return 'https://holibirthday.aoeu2code.com';
 	// return 'http://71.89.76.184';
 	// return 'https://glacial-earth-6398.herokuapp.com';
